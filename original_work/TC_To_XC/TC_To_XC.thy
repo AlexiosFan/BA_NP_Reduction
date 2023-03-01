60aefd4321d5073b098595dba526484594270852
Zixuan Fan <ge43yeb@mytum.de>
2023-03-01 15:54:30 +0100

    corrected the definition of the exact cover and its relating proof

--- original_work/TC_To_XC/TC_To_XC.thy
+++ original_work/TC_To_XC/TC_To_XC.thy
@@ -7,16 +7,65 @@ begin
 
 subsection "Exact cover definitions"
 
-definition "exactCover \<equiv> {(X, S). \<forall>S' \<in> S. (S' \<subseteq> X \<and> (\<forall>x \<in> X. \<forall>S'' \<in> S. x \<in> S' \<and> S' \<noteq> S'' \<longrightarrow> x \<notin> S''))}"
+definition "exactCover \<equiv> 
+   {(X, S). \<Union>S \<subseteq> X \<and>
+    (\<exists>S' \<subseteq> S. \<forall>x \<in> X. \<exists>s \<in> S'. x \<in> s \<and> (\<forall>t \<in> S'. s \<noteq> t \<longrightarrow> x \<notin> t))}"
 
-definition "exactCover' \<equiv> {(X, S). \<forall>S' \<in> S. S' \<subseteq> X \<and> disjoint S}"
+definition "exactCover' \<equiv> {(X, S). \<Union>S \<subseteq> X \<and> (\<exists>S' \<subseteq> S. \<Union>S' = X \<and> disjoint S')}"
 
-lemma "exactCover = exactCover'"
-unfolding exactCover_def exactCover'_def 
-apply auto 
-apply (rule disjointI, blast)
-using disjointD apply blast+
-done 
+lemma "(X, S) \<in> exactCover \<longleftrightarrow> (X, S) \<in> exactCover'"
+proof (standard, goal_cases)
+  assume 1: "(X, S) \<in> exactCover"
+  hence "\<Union>S \<subseteq> X" unfolding exactCover_def by blast
+  from 1 obtain S' where s_def: "S' \<subseteq> S" "\<forall>x \<in> X. \<exists>s \<in> S'. x \<in> s \<and> (\<forall>t \<in> S'. s \<noteq> t \<longrightarrow> x \<notin> t)"
+  unfolding exactCover_def by blast 
+
+  hence "\<forall>x \<in> X. \<exists>s \<in> S'. x \<in> s" by blast   
+  then have "X \<subseteq> \<Union>S'" by blast 
+  moreover have "\<Union>S' \<subseteq> X" using \<open>S' \<subseteq> S\<close> \<open>\<Union>S \<subseteq> X\<close> by blast 
+  ultimately have "X = \<Union>S'" by simp 
+
+  have "disjoint S'"
+  proof (rule disjointI)
+    fix a b 
+    assume ab_def: "a \<in> S'" "b \<in> S'" "a \<noteq> b"
+
+    with s_def have 
+      "\<forall>x \<in> X. x \<in> a \<longrightarrow> x \<notin> b" 
+      "\<forall>x \<in> X. x \<in> b \<longrightarrow> x \<notin> a"
+       by metis+ 
+    hence "\<forall>x\<in>X. x \<notin> a \<inter> b" by blast 
+
+    moreover from \<open>\<Union>S' \<subseteq> X\<close> have "\<forall>x \<in> a \<inter> b. x \<in> X"
+    using ab_def by blast
+
+    ultimately show "a \<inter> b = {}" by blast
+  qed
+
+  from \<open>X = \<Union>S'\<close> \<open>disjoint S'\<close> \<open>S' \<subseteq> S\<close> \<open>\<Union>S \<subseteq> X\<close> show "(X, S) \<in> exactCover'"
+  unfolding exactCover'_def by blast
+next
+  assume 2:"(X, S) \<in> exactCover'"
+  then have "\<Union>S \<subseteq> X" unfolding exactCover'_def by blast
+
+  from 2 obtain S' where S'_def: "S' \<subseteq> S" "\<Union>S' = X" "disjoint S'"
+    unfolding exactCover'_def by blast 
+  
+  have "\<forall>x \<in> X. \<exists>s \<in> S'. x \<in> s \<and> (\<forall>t \<in> S'. s \<noteq> t \<longrightarrow> x \<notin> t)"
+  proof 
+    fix x 
+    assume "x \<in> X"
+    then obtain s where "s \<in> S'" "x \<in> s" using S'_def by blast
+    moreover then have "\<forall>t \<in> S'. s \<noteq> t \<longrightarrow> x \<notin> t"
+      using disjointD[OF \<open>disjoint S'\<close>] by blast
+
+    ultimately show "\<exists>s \<in> S'. x \<in> s \<and> (\<forall>t \<in> S'. s \<noteq> t \<longrightarrow> x \<notin> t)"
+    by blast
+  qed 
+  
+  then show "(X, S) \<in> exactCover" using \<open>\<Union>S \<subseteq> X\<close> \<open>S' \<subseteq> S\<close>
+    unfolding exactCover_def by blast 
+qed
 
 
 
