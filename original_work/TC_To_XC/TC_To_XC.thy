a2a7b5d88e2a6a9794906f2e983e5c872dae0a34
Zixuan Fan <ge43yeb@mytum.de>
2023-03-07 13:12:53 +0100

    updated the a first definition of the reduction and some comments, a refinement is necessary

--- original_work/TC_To_XC/TC_To_XC.thy
+++ original_work/TC_To_XC/TC_To_XC.thy
@@ -7,18 +7,18 @@ begin
 
 subsection "Exact cover definitions"
 
-definition "exactCover \<equiv> 
+definition "exact_cover \<equiv> 
    {(X, S). \<Union>S \<subseteq> X \<and>
     (\<exists>S' \<subseteq> S. \<forall>x \<in> X. \<exists>s \<in> S'. x \<in> s \<and> (\<forall>t \<in> S'. s \<noteq> t \<longrightarrow> x \<notin> t))}"
 
-definition "exactCover' \<equiv> {(X, S). \<Union>S \<subseteq> X \<and> (\<exists>S' \<subseteq> S. \<Union>S' = X \<and> disjoint S')}"
+definition "exact_cover' \<equiv> {(X, S). \<Union>S \<subseteq> X \<and> (\<exists>S' \<subseteq> S. \<Union>S' = X \<and> disjoint S')}"
 
-lemma "(X, S) \<in> exactCover \<longleftrightarrow> (X, S) \<in> exactCover'"
+lemma aux: "(X, S) \<in> exact_cover \<longleftrightarrow> (X, S) \<in> exact_cover'"
 proof (standard, goal_cases)
-  assume 1: "(X, S) \<in> exactCover"
-  hence "\<Union>S \<subseteq> X" unfolding exactCover_def by blast
+  assume 1: "(X, S) \<in> exact_cover"
+  hence "\<Union>S \<subseteq> X" unfolding exact_cover_def by blast
   from 1 obtain S' where s_def: "S' \<subseteq> S" "\<forall>x \<in> X. \<exists>s \<in> S'. x \<in> s \<and> (\<forall>t \<in> S'. s \<noteq> t \<longrightarrow> x \<notin> t)"
-  unfolding exactCover_def by blast 
+  unfolding exact_cover_def by blast 
 
   hence "\<forall>x \<in> X. \<exists>s \<in> S'. x \<in> s" by blast   
   then have "X \<subseteq> \<Union>S'" by blast 
@@ -42,14 +42,14 @@ proof (standard, goal_cases)
     ultimately show "a \<inter> b = {}" by blast
   qed
 
-  from \<open>X = \<Union>S'\<close> \<open>disjoint S'\<close> \<open>S' \<subseteq> S\<close> \<open>\<Union>S \<subseteq> X\<close> show "(X, S) \<in> exactCover'"
-  unfolding exactCover'_def by blast
+  from \<open>X = \<Union>S'\<close> \<open>disjoint S'\<close> \<open>S' \<subseteq> S\<close> \<open>\<Union>S \<subseteq> X\<close> show "(X, S) \<in> exact_cover'"
+  unfolding exact_cover'_def by blast
 next
-  assume 2:"(X, S) \<in> exactCover'"
-  then have "\<Union>S \<subseteq> X" unfolding exactCover'_def by blast
+  assume 2:"(X, S) \<in> exact_cover'"
+  then have "\<Union>S \<subseteq> X" unfolding exact_cover'_def by blast
 
   from 2 obtain S' where S'_def: "S' \<subseteq> S" "\<Union>S' = X" "disjoint S'"
-    unfolding exactCover'_def by blast 
+    unfolding exact_cover'_def by blast 
   
   have "\<forall>x \<in> X. \<exists>s \<in> S'. x \<in> s \<and> (\<forall>t \<in> S'. s \<noteq> t \<longrightarrow> x \<notin> t)"
   proof 
@@ -63,10 +63,44 @@ next
     by blast
   qed 
   
-  then show "(X, S) \<in> exactCover" using \<open>\<Union>S \<subseteq> X\<close> \<open>S' \<subseteq> S\<close>
-    unfolding exactCover_def by blast 
+  then show "(X, S) \<in> exact_cover" using \<open>\<Union>S \<subseteq> X\<close> \<open>S' \<subseteq> S\<close>
+    unfolding exact_cover_def by blast 
 qed
 
+corollary "exact_cover = exact_cover'"
+   using aux by auto
 
+(*2 different definitions, which is better?
+  Possibility to reuse the Disjoint_Sets*)
 
+lemma exact_coverI: "\<lbrakk>S' \<subseteq> S; \<Union>S = X; disjoint S'; \<Union>S' = X\<rbrakk> \<Longrightarrow> (X, S) \<in> exact_cover'"
+  unfolding exact_cover'_def by blast
+
+definition "TC_XC E \<equiv> (if ugraph E 
+  then (let X = {(v, opt, c). v \<in> \<Union>E \<and> (opt = None \<or> (\<exists>u. {v, u} \<in> E \<longrightarrow> opt = Some u)) \<and> c \<in> {0, 1, 2}}
+                \<union> {(v, opt, c). v \<in> \<Union>E \<and> opt = Some v \<and> c = 3};
+            S = {{u_trip, p} | u_trip p. u_trip \<noteq> p \<and> u_trip \<in> X \<and> (\<exists>u c. u_trip = (u, Some u, 3) \<and> p = (u, None, c))}
+                \<union> {{p1, p2} | p1 p2. p1 \<noteq> p2 \<and> p1 \<in> X \<and> p2 \<in> X \<and> (\<exists>u v c1 c2. p1 = (v, Some u, c1) \<and> p2 = (u, Some v, c2) \<and> c1 \<noteq> c2)}
+                \<union> {{(v, opt, c). opt = None \<or> (\<exists>u. {v, u} \<in> E \<longrightarrow> opt = Some u) \<and> c \<in> {0, 1, 2}} | v. v \<in> \<Union>E}
+             in (X,S))
+  else ({}, {}))"
+
+(*refinement necessary*)
+
+lemma TC_XC_sound: "E \<in> three_colorability \<Longrightarrow> (TC_XC E) \<in> exact_cover'"
+sorry
+
+(*proof of soundness understood, problem and refinement goal: 
+the definition of \<chi> does not involve a set of colors, but a direct partition of vertexes*)
+
+lemma TC_XC_complete: "(TC_XC E) \<in> exact_cover' \<Longrightarrow> E \<in> three_colorability"
+sorry
+
+lemma "is_reduction TC_XC three_colorability exact_cover'"
+using TC_XC_sound TC_XC_complete unfolding is_reduction_def by blast
+
+(*proof of completeness understood, problem: the proof far away from the definition of
+the three coloarbility, a formalisation and abstraction is necessary*)
+
+thm is_reduction_def
 end 
